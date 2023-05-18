bb7868141e9aa73ec2050ea5b1c4c7c4443c78f3
Cosm02 <amirulkayes444@gmail.com>
2023-05-19 02:09:39 +0600

    burger

---
+++ pages/product/[slug].js
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React, { useContext } from 'react';
+import { toast } from 'react-toastify';
+import Layout from '../../components/Layout';
+import Product from '../../models/Product';
+import db from '../../utils/db';
+import { Store } from '../../utils/Store';
+
+export default function ProductScreen(props) {
+  const { product } = props;
+  const { state, dispatch } = useContext(Store);
+  const router = useRouter();
+  if (!product) {
+    return <Layout title="Produt Not Found">Produt Not Found</Layout>;
+  }
+
+  const addToCartHandler = async () => {
+    const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+
+    if (data.countInStock < quantity) {
+      return toast.error('Sorry. Product is out of stock');
+    }
+
+    dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
+  };
+
+  return (
+    <Layout title={product.name}>
+      <div
+        className="grid grid-cols-1 gap-6 
+      md:grid-cols-3 lg:grid-cols-3 my-20"
+      >
+        <div className="">
+          <img
+            src={product.image}
+            alt={product.name}
+            width={600}
+            height={600}
+            layout="responsive"
+          />
+        </div>
+        <div>
+          <div className="card p-5">
+            <div className="mb-2 flex justify-between">
+              <div>Price</div>
+              <div>${product.price}</div>
+            </div>
+            <div className="mb-2 flex justify-between">
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
+            </div>
+            <button
+              className="primary-button w-full"
+              onClick={addToCartHandler}
+            >
+              Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="description">
+        <ul className="mb-2">
+          <li>
+            <h1 className="text-lg">{product.name}</h1>
+          </li>
+          <li>Category: {product.category}</li>
+
+          <li>
+            {product.rating} of {product.numReviews} reviews
+          </li>
+        </ul>
+        <li>Description: {product.description}</li>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: product ? db.convertDocToObj(product) : null,
+    },
+  };
+}
